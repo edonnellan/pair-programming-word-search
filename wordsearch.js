f51995931964e374293236bd4cbcc910c51dec1e
EDonnellan <elaine.donny@gmail.com>
2023-02-16 00:11:45 +0000

    WordSearch task. Functions are passed a 2d array and the word to find. worked with JWall.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require("../../async/matrix_transposition");
 
-module.exports = wordSearch
+const check = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let row of horizontalJoin) {
+        if (row.includes(word)) return true
+    };
+    return false;
+};
+
+const wordSearch = (letters, word) => { 
+    
+    if(check(letters, word)) {
+        return true
+    };
+
+    if(check(transpose(letters), word)) {
+        return true;
+    };
+    return false; 
+};
+
+module.exports = wordSearch;
+
